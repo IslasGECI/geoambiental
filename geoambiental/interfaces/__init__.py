b73446bd8e9443f0f43a0f50f930d44651f0b290
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-05-28 11:01:43 -0700

    Agrega primer versión de las interfaces

---
+++ geoambiental/interfaces/__init__.py
@@ -0,0 +1,5 @@
+from .IGeoRaster import IGeoRaster
+from .IGeoReference import IGeoReference
+from .IGeoReferenceBounded import IGeoReferenceBounded
+from .IGeoReferenceFinite import IGeoReferenceFinite
+from .ITimeReference import ITimeReference
