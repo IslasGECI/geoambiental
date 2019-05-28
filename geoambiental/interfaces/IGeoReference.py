963709f2abd12926ef398e84e07374090f272551
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-05-28 11:57:43 -0700

    Agrega breve descripción a interfaz IGeoReference

--- geoambiental/interfaces/IGeoReference.py
+++ geoambiental/interfaces/IGeoReference.py
@@ -2,6 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class IGeoReference(ABC):
+    """
+    Esta interfaz las deben cumplir todos aquellos objetos tengan coordenadas
+    georeferenciada, no está pensada para rejillas de coordenadas
+    """
     @property
     @abstractmethod
     def x(self) ->float:
