d92f0ee4d49be09ada2dc2af09927ca4c7bceb66
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-05-28 12:02:23 -0700

    Agrega descripción de la interfaz ITimeReference

--- geoambiental/interfaces/ITimeReference.py
+++ geoambiental/interfaces/ITimeReference.py
@@ -2,6 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class ITimeReference(ABC):
+    """
+    Esta interfaz la deben cumplir todas aquellas clases que representen objetos
+    que tengan una referencia temporal
+    """
     @property
     @abstractmethod
     def date(self):
