9679f03159c72541df70958cd4b4885d58221c63
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-05-28 16:36:22 -0700

    Agrega prueba para verificar zona utm

--- geoambiental/tests/test_Point.py
+++ geoambiental/tests/test_Point.py
@@ -1,6 +1,8 @@
 import unittest
 import json
 
+import numpy as np
+
 from .. import Point
 
 class TestPoint(unittest.TestCase):
@@ -11,6 +13,10 @@ class TestPoint(unittest.TestCase):
     def test_utm_coordinate(self):
         self.assertAlmostEqual(self.p.x, 371938.22957668)
         self.assertAlmostEqual(self.p.y, 2549601.77459413)
+
+    def test_utm_zone(self):
+        self.assertEqual(self.p.utm_zone[0], "11")
+        self.assertEqual(self.p.utm_zone[1], "Q")
 
     
 if __name__ == '__main__':
