1dc50f29bae4d97bae736b65f15adb0d6bc92290
Efren Cabrera <efren507.507@gmail.com>
2019-06-20 12:31:05 -0700

    Corrige orden en que se importan los submódulos
    La clase Line depende de las funciones definidas en .operations por lo tanto se debe importar primero

--- geoambiental/__init__.py
+++ geoambiental/__init__.py
@@ -1,5 +1,6 @@
 from .interfaces import *
+from .operations import *
+
 from .Line import Line
-from .operations import distance_between_points_km, distance_between_points_m
 from .Point import Point
 from .PointArray import PointArray
