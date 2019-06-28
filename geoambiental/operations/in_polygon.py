b125dc46199af8f7d80f0b415799c471e0fbdeae
Jesus Efren Cabrera Ortega <efren.cabrera@islas.org.mx>
2019-06-27 17:19:37 -0700

    Corrige nombre de la variable
    Cambia el nombre de la variable para que sea más descriptivo

--- geoambiental/operations/in_polygon.py
+++ geoambiental/operations/in_polygon.py
@@ -4,10 +4,10 @@ from ..interfaces.IGeoReference import IGeoReference
 from ..interfaces.IGeoReferenceBounded import IGeoReferenceBounded
 
 
-def in_polygon(geo_reference: IGeoReference, polygon: IGeoReferenceBounded):
+def in_polygon(geo_element: IGeoReference, polygon: IGeoReferenceBounded):
     es_dentro = []
     poligono = path.Path(
         [coordenada for coordenada in zip(polygon.lon, polygon.lat)])
-    for lon, lat in zip(geo_reference.lon, geo_reference.lat):
+    for lon, lat in zip(geo_element.lon, geo_element.lat):
         es_dentro.append(poligono.contains_points([(lon, lat)])[0])
     return es_dentro
