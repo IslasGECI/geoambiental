a7eb483dbb8bfeee977c11a4d25e0e5b98cd3298
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-08-06 12:17:13 -0700

    Agrega clases restantes al namespace del módulo

--- geoambiental/__init__.py
+++ geoambiental/__init__.py
@@ -4,8 +4,12 @@ from .operations import *
 from .Point import Point
 from .PointArray import PointArray
 
+from .Grid import Grid
 from .Line import Line
 from .Polygon import Polygon
 
+from .Field import Field
 from .GeoCircle import GeoCircle
+from .Map import Map
+from .PolygonArray import PolygonArray
 from .Trajectory import Trajectory
